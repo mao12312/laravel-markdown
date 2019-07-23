2ca194fb7a6ca69d03d583516008ea8464453d80
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-07-23 19:31:03 +0900

    add text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +18,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+
 </head>
 <body>
     <div id="app">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,38 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
-    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+@section('content')
+    <div class="container">
+        <textarea>
+### 前提・実現したいこと
 
-    <title>Document</title>
-</head>
-<body>
-<textarea id="editor" name="name" rows="8" cols="40"></textarea>
-<input id="filename" type="text" placeholder="input file name" >
-<a class="button" id="download" href="#" download="text.md" onclick="handleDownload()">Download</a>
+ここに質問の内容を詳しく書いてください。
+（例）PHP(CakePHP)で●●なシステムを作っています。
+■■な機能を実装中に以下のエラーメッセージが発生しました。
 
-<script>
-    var simplemde = new SimpleMDE({
-        element: document.getElementById("editor")
-    });
-</script>
-</body>
-</html>
+### 発生している問題・エラーメッセージ
+
+```
+エラーメッセージ
+```
+
+### 該当のソースコード
+
+```ここに言語名を入力
+ソースコード
+```
+
+### 試したこと
+
+ここに問題に対して試したことを記載してください。
+
+### 補足情報（FW/ツールのバージョンなど）
+
+ここにより詳細な情報を記載してください。
+        </textarea>
+    </div>
+    <script>
+        var simplemde = new SimpleMDE({
+
+        });
+    </script>
+@endsection
