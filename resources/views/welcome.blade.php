7f12093fe8ce1a9c4b9eff151349e3ee03cbdec1
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-07-24 23:21:15 +0900

    add posts and comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,25 @@
 @extends('layouts.app')
 
+@section('head')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+@endsection
+
 @section('content')
     <div class="container">
+        <h2>投稿フォーム</h2>
         <textarea>
 ### 前提・実現したいこと
 
 ここに質問の内容を詳しく書いてください。
-（例）PHP(CakePHP)で●●なシステムを作っています。
-■■な機能を実装中に以下のエラーメッセージが発生しました。
 
-### 発生している問題・エラーメッセージ
-
-```
-エラーメッセージ
-```
-
-### 該当のソースコード
-
-```ここに言語名を入力
-ソースコード
-```
+### 発生している問題
 
 ### 試したこと
 
 ここに問題に対して試したことを記載してください。
 
-### 補足情報（FW/ツールのバージョンなど）
-
-ここにより詳細な情報を記載してください。
+### ここにより詳細な情報を記載してください。
         </textarea>
     </div>
     <script>
